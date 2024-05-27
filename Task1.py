5d341ff66d35c6a62a92e695a4579c8f92f72963
An <ache0159@student.monash.edu>
2024-05-27 15:15:52 +1000

    task 1

--- Task1.py
+++ Task1.py
@@ -1,1 +1,19 @@
-test
+# Author: An Cheng
+# Date: 27/05/24
+# -----------------------------
+# Description: Given a list of digits, determine the integer that it represents
+#              For example, given the list: [8,3,5,1],
+#              the program will output 8351 as an integer.
+
+# main function to determine the interger it represents
+# usually we will have one main function for each program
+def main():
+    given_list = [8, 3, 5, 1] # given list
+    output = 0 # a integer variable that hold the output
+    for digit in given_list: # add each digit to the end
+        output = output*10 + digit
+    print(output) # print the output
+
+# when running this file, this will call the main function
+if __name__ == "__main__":
+    main() # call the main function
